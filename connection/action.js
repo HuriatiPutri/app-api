97f576d3c9aec21ad0b098d7674d522185d919d4
Putri Huriati <putrihuriati7@gmail.com>
2023-12-18 23:21:26 +0700

    fix: sql query

--- connection/action.js
+++ connection/action.js
@@ -5,8 +5,8 @@ const bcrypt = require("bcrypt")
 
 async function getBalance(userId){
     const sqlSearch = await db.query(`SELECT * FROM balances left join users on 
-    users.id = balance.user_id
-    WHERE balance.user_id ='${userId}'`);
+    users.id = balances.user_id
+    WHERE balances.user_id ='${userId}'`);
 
     const data = helper.emptyOrRows(sqlSearch);
     if(data.length > 0) {
